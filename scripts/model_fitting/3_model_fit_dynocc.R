2cafd39419158967f13a477dfbd5d669e251f350
Kristin Davis <kristin.p.davis@gmail.com>
2025-03-14 12:31:12 -0600

    Remove model diagnostics and put in a separate script

--- scripts/model_fitting/3_model_fit_dynocc.R
+++ scripts/model_fitting/3_model_fit_dynocc.R
@@ -1,4 +1,4 @@
-# Purpose: Fit and summarize output / calculate model diagnostics from the dynamic occupancy model for Eastern Screech-Owl in Fort Collins, CO
+# Purpose: Fit the dynamic occupancy model for Eastern Screech-Owl in Fort Collins, CO
 # Author: Kristin P. Davis
 
 # Note: JAGS must be installed on one's computer to fit the model. Download JAGS at https://sourceforge.net/projects/mcmc-jags/
@@ -117,27 +117,5 @@ if (length(monitor_params) > 4) {
   saveRDS(mod_out,
           file = here("output", "mod_out_auc.rds"))
 }
-
-
-# Model diagnostics ----
-## Bayesian p value for log likelihood
-mod_out_occ <- readRDS(here("output", "mod_out_occ.rds"))
-mod_out_occ_jags_sum <- add.summary(mod_out_occ, confidence = c(0.95))
-mod_out_occ <- data.frame(mod_out_occ_jags_sum$summaries)
-
-(bpv_loglik <- mod_out_occ |>
-  rownames_to_column() |>
-  rename(param = rowname) |>
-  filter(param == "log.bpv") |>
-  pull(Mean))
-
-## Calculate area under the receiver operator curve (AUC) ----
-## Calculate AUC for the prevalence process (i.e., at the level of the survey)
-mod_out_auc <- readRDS(here("output", "mod_out_auc.rds"))
-pocc_samples <- as.matrix(mod_out_auc$mcmc)
-pocc_mn <- data.frame(pocc = colnames(pocc_samples),
-                      mn = apply(pocc_samples, 2, mean))
-y_obs <- easo_obs$presence
-(auc.prev.pocc <- round(as.numeric(roc(y_obs, pocc_mn$mn)$auc), 3)) # compares the observed data to data generated from the model
 
 # end script
